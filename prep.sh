9df72e791b79cbb16fd67717e517492facc9ab9c
Carlos Hernandez <carlos@uncorkgroup.com>
2019-05-03 19:24:50 -0400

    added logic to handle different sed calls between Mac and Linux

--- prep.sh
+++ prep.sh
@@ -1,12 +1,22 @@
 #! /bin/sh
-sed -i '' 's|"ckanAPI": window.location.protocol + "//" + ckan + "/api/3/action/", "ckanURL": window.location.protocol + "//" + ckan|"ckanAPI":"http://localhost:5000/api/3/action/", "ckanURL":"http://localhost:5000"|g' ../wp-open-data-toronto/wp-open-data-toronto/js/utils.js
-
+echo "STARTED: Preparing Open Data configuration files"
 cp docker-compose.yml ../docker/ckan/contrib/docker/docker-compose.yml  && \
 cp ../docker/ckan/contrib/docker/.env.template ../docker/ckan/contrib/docker/.env
 
-case "$OSTYPE" in
-    darwin*)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "FOUND: Running on Macos. Configuring files for MacOS."
+    sed -i '' 's|"ckanAPI": window.location.protocol + "//" + ckan + "/api/3/action/", "ckanURL": window.location.protocol + "//" + ckan|"ckanAPI":"http://localhost:5000/api/3/action/", "ckanURL":"http://localhost:5000"|g' ../wp-open-data-toronto/wp-open-data-toronto/js/utils.js && \
+    echo "UPDATED: CKAN URL in WordPress utils.js will point to localhost:5000"
+    
     sed -i '' 's,CKAN_SITE_URL=http://localhost:5000,# CKAN_SITE_URL=http://localhost:5000,g' ../docker/ckan/contrib/docker/.env && \
     sed -i '' 's,# CKAN_SITE_URL=http://docker.for.mac.localhost:5000,CKAN_SITE_URL=http://docker.for.mac.localhost:5000,g' ../docker/ckan/contrib/docker/.env
-    ;;
-esac
+    echo "UPDATED: .env file CKAN_SITE_URL set to http://docker.for.mac.localhost:5000"
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "FOUND: Running on Linux. Configuring files for Linux."
+    sed -i 's|"ckanAPI": window.location.protocol + "//" + ckan + "/api/3/action/", "ckanURL": window.location.protocol + "//" + ckan|"ckanAPI":"http://localhost:5000/api/3/action/", "ckanURL":"http://localhost:5000"|g' ../wp-open-data-toronto/wp-open-data-toronto/js/utils.js
+    echo "UPDATED: CKAN URL in WordPress utils.js will point to localhost:5000"
+
+fi
+
+echo "FINISHED: Preparation complete. Please proceed to the next step."
