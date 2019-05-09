59811c1643488bfbfefa390b70b1c79867984a8e
Carlos Hernandez <carlos@uncorkgroup.com>
2019-05-09 16:25:13 -0400

    Now goes into directory before running init script
    This was necessary to be able to find the files

--- scripts/ckan_init.sh
+++ scripts/ckan_init.sh
@@ -10,7 +10,10 @@ virtualenv setup
   pip install -U pip
   pip install ckanapi geopandas
 
-  python /open-data-workspace/docker-local-environment/ckan_init/ckan_init.py $1
+  cd /open-data-workspace/docker-local-environment/ckan_init/
+
+  python ckan_init.py $1
+  
 } || {
   echo 'Unable to initialize Python environment'
 }
