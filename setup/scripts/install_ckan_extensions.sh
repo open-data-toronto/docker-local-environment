104aa29706a6657593473d941c7f54b57d3d63cd
Carlos Hernandez <carlos@uncork.ai>
2020-05-07 14:16:29 -0400

    Adding visualization extension for validation

--- setup/scripts/install_ckan_extensions.sh
+++ setup/scripts/install_ckan_extensions.sh
@@ -6,6 +6,7 @@
 . /usr/lib/ckan/venv/bin/activate && \
 cd /usr/lib/ckan/venv/src && \
 pip install -e "git+https://github.com/ckan/ckanext-spatial.git#egg=ckanext-spatial" && \
+pip install -e "git+https://github.com/keitaroinc/ckanext-visualize.git#egg=ckanext-visualize" && \
 pip install -r ckanext-spatial/pip-requirements.txt && \
 pip install ckanext-geoview && \
 pip install -e ckanext-opendatatoronto && \
